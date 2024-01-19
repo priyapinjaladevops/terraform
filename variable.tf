3cd1ee18a0bcd68625d029b4834775b201351b89
priyapinjaladevops <147639356+priyapinjaladevops@users.noreply.github.com>
2024-01-19 20:25:58 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,21 @@
-variable "ami" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "subnet_id" {}
+provider "aws" {
+	region = "ap-south-1"
+	access_key = "AKIA2UPMVTFCO3IHWQFP"
+	secret_key     = "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"
+}
+
+variable "ami" {
+default = "ami-0763cf792771fe1bd"
+}
+
+variable "instance_type" {
+default = "t2.nano"
+}
+
+variable "key_name" {
+default = "centos"
+}
+
+variable "subnet_id" {
+default = "subnet-08a97afb027a05e5c"
+}
